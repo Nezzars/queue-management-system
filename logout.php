38e1cd1a3f5d04e4bcd2fcaa33493f1a044b8f66
dilucwgs <dilucwgs123@gmail.com>
2023-08-06 14:27:03 +0800

    asdf

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
 // Perform the logout action here (e.g., destroying session, clearing cookies, etc.)
 
 // Redirect the user back to the login page after logout (replace login.php with the actual login page URL)
+//GITHUB
 session_destroy();
 header("Location: login/login.php");
 exit;
